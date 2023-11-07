c8b3a339a4a893e9aeb317b7f81f0fda143b2f0c
Jose De Los Reyes <jddelosreyes23@hotmail.com>
2023-11-06 19:02:02 -0500

    header style working

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Landing from './views/landing/index.jsx'
 import List from './views/list/index';
+import Header from './components/layout/header.jsx'
 
 export default function App() {
   return (
     <>
+      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
